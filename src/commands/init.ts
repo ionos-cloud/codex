4bb7540810e4b08156bc03218d7838fff51329e0
Florin Mihalache <florin.mihalache@gmail.com>
2021-03-14 00:45:18 +0200

    vdc-host

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -2,6 +2,7 @@ import {Command, flags} from '@oclif/command'
 
 import { VersionData } from '../services/version-data'
 import runConfig from '../services/run-config'
+import vdc from '../services/vdc';
 
 export default class Init extends Command {
   static description = 'initialize a swagman project in the current directory'
@@ -13,12 +14,18 @@ export default class Init extends Command {
   static flags = {
     help: flags.help({char: 'h'}),
     version: flags.integer({char: 'v', default: VersionData.defaultVersion}),
-    debug: flags.boolean({char: 'd', default: false})
+    debug: flags.boolean({char: 'd', default: false}),
+    'vdc-host': flags.string({description: 'vdc host'})
   }
 
   async run() {
     const {flags} = this.parse(Init)
     runConfig.debug = flags.debug
+
+    if (flags['vdc-host'] !== undefined) {
+      vdc.host = flags['vdc-host']
+    }
+
     const versionData = new VersionData(flags.version)
     await versionData.init()
   }
